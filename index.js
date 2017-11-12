1f31de61b19d26335ee48c2fe2000ec7c0e4c56e
Sean D <sdoherty8@icloud.com>
2017-11-12 20:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function BoardMember(name, homeState, training
+function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
+}
 
 BoardMember.prototype.veto = function()
