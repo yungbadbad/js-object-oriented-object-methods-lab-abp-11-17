b4c5a1f1ef716b1fb539d9efd2a3843512c5b01c
Sean D <sdoherty8@icloud.com>
2017-11-12 20:41:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function BoardMember(name, homeState, training){
   this.training = training
 }
 
-BoardMember.prototype.veto = function()
+BoardMember.prototype.veto = function(){
+  returns 'No, I must disagree'
+}
